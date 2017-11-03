4e4e0eb17a8683b983371113942bb49863e4b6ff
Dirk Weil <dirk.weil@gedoplan.de>
2017-11-03 10:23:52 +0100

    Event um Auswahl des Raumes ergänzt

--- src/main/java/de/gedoplan/event/domain/Event.java
+++ src/main/java/de/gedoplan/event/domain/Event.java
@@ -6,17 +6,26 @@ import de.gedoplan.common.domain.ZonedInterval;
 import de.gedoplan.event.domain.attribute.EventName;
 import de.gedoplan.person.domain.Person;
 import de.gedoplan.room.domain.Room;
+import de.gedoplan.room.domain.RoomOccupancy;
+import de.gedoplan.room.domain.RoomRepository;
 
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.inject.Inject;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+
+import org.jboss.weld.exceptions.IllegalArgumentException;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -49,12 +58,32 @@ public class Event extends GeneratedLongIdEntity {
   @ManyToOne
   private Room room;
 
+  @Inject
+  @Transient
+  RoomRepository roomRepository;
+
   public Event(EventName name, SeatCapacity capacity, ZonedInterval interval) {
+    List<Room> freeRooms = this.roomRepository.findFree(interval, capacity);
+    if (freeRooms.isEmpty()) {
+      throw new IllegalArgumentException("No room available");
+    }
+
     this.name = name;
     this.interval = interval;
     this.capacity = capacity;
 
+    Comparator<Room> rc = new Comparator<Room>() {
+      @Override
+      public int compare(Room r1, Room r2) {
+        return r1.getCapacity().compareTo(r2.getCapacity());
+      }
+    };
+    this.room = freeRooms.stream().collect(Collectors.minBy(rc)).get();
+
     this.participants = new HashSet<>();
+
+    // TODO: Änderung in Room OK?
+    this.room.getOccupancies().add(new RoomOccupancy(this.interval, name.getValue()));
   }
 
 }
